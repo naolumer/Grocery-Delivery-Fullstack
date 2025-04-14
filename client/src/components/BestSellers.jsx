62efc1193508db893df0e622c5df5b0efb480e2a
naolumer <naolumer3@gmail.com>
2025-04-14 21:56:04 +0300

    best sellers section

--- client/src/components/BestSellers.jsx
+++ client/src/components/BestSellers.jsx
@@ -8,12 +8,11 @@ const BestSellers = () => {
           <h1 className='text-3xl font-medium  text-gray-700'>Best Sellers</h1>
           <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 pt-12 mb-[113px]'>
               {
-                  dummyProducts.slice(3,8).map((item,index)=> (
+                 dummyProducts.slice(3,8).map((item,index)=> (
                     <FoodCard item={item} index={index}/>
-                  ))
+                ))
               }
           </div>
-  
       </div>
     )
 }
