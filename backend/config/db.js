d945d92ab3698fa759fae17b2fd0f23649088229
naolumer <naolumer3@gmail.com>
2025-04-14 21:51:33 +0300

    connect database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,5 +4,4 @@ const connectDB = async () => {
     mongoose.connection.on("connected", ()=>console.log("Database Connected"))
     await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
 }
-
 export default connectDB
