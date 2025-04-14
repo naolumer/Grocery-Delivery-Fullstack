3d33f6ff559d664bc802e1c96981cec4f0d45270
naolumer <naolumer3@gmail.com>
2025-04-14 21:56:52 +0300

    created category component

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -4,25 +4,24 @@ import { AppContext } from '../context/AppContext'
 import { Link } from 'react-router-dom'
 
 
-
 const Categories = () => {
 
   const {setCategory} = useContext(AppContext)
 
   return (
-    <div className='mt-[60px] w-[92%] md:w-[85%] mx-auto text-gray-700 font-medium mb-6'> 
+  <div className='mt-[60px] w-[92%] md:w-[85%] mx-auto text-gray-700 font-medium mb-6'> 
     <h1 className='text-3xl font-medium mb-6'>Categories</h1>
     <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6'> 
         {
             categories.map((categ,index)=> (
-                <Link key={index} to={`/products/${categ.path}`} onClick={()=>{setCategory(categ.path); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className='justify-center items-center group px-3 py-5 rounded-lg flex flex-col gap-2' style={{ backgroundColor: categ.bgColor }} key={categ.title}>
-                    <img  className='max-w-28 group-hover:scale-110 transition-all duration-100' src={categ.image} alt="" />
-                    <p className=' text-sm'>{categ.text}</p>
-                </Link>
+              <Link key={index} to={`/products/${categ.path}`} onClick={()=>{setCategory(categ.path); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className='justify-center items-center group px-3 py-5 rounded-lg flex flex-col gap-2' style={{ backgroundColor: categ.bgColor }} key={categ.title}>
+                  <img  className='max-w-28 group-hover:scale-110 transition-all duration-100' src={categ.image} alt="" />
+                  <p className=' text-sm'>{categ.text}</p>
+              </Link>
             ))
         }
     </div>
-    </div>
+  </div>
   )
 }
 
