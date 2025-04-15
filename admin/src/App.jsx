9738082e6005e1f0157b708858a861ab83f7af6f
naolumer <naolumer3@gmail.com>
2025-04-15 19:35:00 +0300

    navbar component for admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,10 +3,13 @@ import { Toaster } from 'react-hot-toast'
 import { Route,Routes } from 'react-router-dom'
 import Orders from './pages/Orders.jsx'
 import ProductsList from './pages/ProductsList.jsx'
+import Navbar from './components/Navbar.jsx'
+import AddProduct from "./pages/AddProduct.jsx"
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/add-product" element={<AddProduct/>} />
         <Route path='/orders' element={<Orders/>}/>
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { assets } from '../../../client/src/assets/assets'
+
 
 const Navbar = () => {
+    const goToFrontend = () => {
+        window.location.href = 'http://localhost:5173/'
+      }
+
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between py-3 w-full px-3 md:pr-8 border-b border-gray-300'>
+        <img
+            onClick={goToFrontend}
+            className='md:w-[155px] w-[130px] ml-3 md:ml-5 lg:ml-12 cursor-pointer items-center'
+            src={assets.logo}
+            alt=""
+        />
+        <div className='flex items-center gap-3'>
+            <p className='text-gray-500 text-[17px]'>Hi admin!</p>
+            <button className='text-gray-500 text-sm rounded-full border border-gray-500 px-4 py-1'>Logout</button>
+        </div>
+    </div>
+    
   )
 }
 
