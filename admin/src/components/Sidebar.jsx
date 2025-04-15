851848d341a44c52f74c1e6f76b15c9fac1fcddf
naolumer <naolumer3@gmail.com>
2025-04-15 20:29:44 +0300

    added active class on link elements

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../../client/src/assets/assets'
 import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState('add-product') // default active
+
+  const baseClass = 'flex items-center w-full gap-3 pl-3 py-3 hover:bg-gray-100'
+  const isActiveClass = 'bg-[#e5f4ea] border-r-[5px] border-[#3cc489]'
+
   return (
-    <div className='h-[91vh] w-64 absolute left-0
-     top-[60px] z-10 border-r bg-white border-gray-300 pt-5'>
-    <div className='flex flex-col gap-6 ml-4 '>
-        <Link to="/add-product" className="flex items-center w-full gap-3">
-            <img  className='w-7 h-7' src={assets.add_icon} alt="" />
-            <p className=' text-gray-700'>Add Product</p>
+    <div className='h-[91vh] w-64 absolute left-0 top-[60px] z-10 border-r bg-white border-gray-300 pt-3'>
+      <div className='flex flex-col w-full gap-1'>
+        <Link
+          to="/add-product"
+          onClick={() => setActiveItem('add-product')}
+          className={`${baseClass} ${activeItem === 'add-product' ? isActiveClass : ''}`}
+        >
+          <img className='w-7 h-7' src={assets.add_icon} alt="" />
+          <p className='text-gray-700'>Add Product</p>
         </Link>
-        <Link to="/product-list" className="flex w-full items-center gap-3">
-            <img className='w-7 h-7' src={assets.product_list_icon} alt="" />
-            <p className=' text-gray-700'>Product List</p>
+
+        <Link
+          to="/product-list"
+          onClick={() => setActiveItem('product-list')}
+          className={`${baseClass} ${activeItem === 'product-list' ? isActiveClass : ''}`}
+        >
+          <img className='w-7 h-7' src={assets.product_list_icon} alt="" />
+          <p className='text-gray-700'>Product List</p>
         </Link>
-        <Link to="/orders" className="flex w-full items-center gap-3">
-            <img  className='w-7 h-7'src={assets.order_icon} alt="" />
-            <p className=' text-gray-700'>Orders</p>
+
+        <Link
+          to="/orders"
+          onClick={() => setActiveItem('orders')}
+          className={`${baseClass} ${activeItem === 'orders' ? isActiveClass : ''}`}
+        >
+          <img className='w-7 h-7' src={assets.order_icon} alt="" />
+          <p className='text-gray-700'>Orders</p>
         </Link>
-    </div>
+      </div>
     </div>
   )
 }
