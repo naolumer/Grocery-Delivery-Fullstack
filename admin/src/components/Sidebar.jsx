c0a4a305a8543fbe8b8bd58f25554f70b4112a91
naolumer <naolumer3@gmail.com>
2025-04-15 20:40:32 +0300

    made the sidebar responsive

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -5,37 +5,37 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
   const [activeItem, setActiveItem] = useState('add-product') // default active
 
-  const baseClass = 'flex items-center w-full gap-3 pl-3 py-3 hover:bg-gray-100'
+  const baseClass = 'flex items-center w-full gap-3 pl-3 py-3 '
   const isActiveClass = 'bg-[#e5f4ea] border-r-[5px] border-[#3cc489]'
 
   return (
-    <div className='h-[91vh] w-64 absolute left-0 top-[60px] z-10 border-r bg-white border-gray-300 pt-3'>
+    <div className='h-[91vh] w-[60px] md:w-64 absolute left-0 top-[60px] z-10 border-r bg-white border-gray-300 pt-3'>
       <div className='flex flex-col w-full gap-1'>
         <Link
           to="/add-product"
           onClick={() => setActiveItem('add-product')}
-          className={`${baseClass} ${activeItem === 'add-product' ? isActiveClass : ''}`}
+          className={`${baseClass} ${activeItem === 'add-product' ? isActiveClass : 'hover:bg-gray-100'}`}
         >
           <img className='w-7 h-7' src={assets.add_icon} alt="" />
-          <p className='text-gray-700'>Add Product</p>
+          <p className='text-gray-700 hidden md:inline-block'>Add Product</p>
         </Link>
 
         <Link
           to="/product-list"
           onClick={() => setActiveItem('product-list')}
-          className={`${baseClass} ${activeItem === 'product-list' ? isActiveClass : ''}`}
+          className={`${baseClass} ${activeItem === 'product-list' ? isActiveClass : 'hover:bg-gray-100'}`}
         >
           <img className='w-7 h-7' src={assets.product_list_icon} alt="" />
-          <p className='text-gray-700'>Product List</p>
+          <p className='text-gray-700 hidden md:inline-block'>Product List</p>
         </Link>
 
         <Link
           to="/orders"
           onClick={() => setActiveItem('orders')}
-          className={`${baseClass} ${activeItem === 'orders' ? isActiveClass : ''}`}
+          className={`${baseClass} ${activeItem === 'orders' ? isActiveClass : 'hover:bg-gray-100'}`}
         >
           <img className='w-7 h-7' src={assets.order_icon} alt="" />
-          <p className='text-gray-700'>Orders</p>
+          <p className='text-gray-700 hidden md:inline-block'>Orders</p>
         </Link>
       </div>
     </div>
