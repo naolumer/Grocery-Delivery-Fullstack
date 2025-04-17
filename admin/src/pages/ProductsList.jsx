7fea51cedf21029b93e17a934e322fe923e2bf16
naolumer <naolumer3@gmail.com>
2025-04-17 21:21:50 +0300

    imported useState

--- admin/src/pages/ProductsList.jsx
+++ admin/src/pages/ProductsList.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import axios from "axios"
+import { useState } from 'react'
+
 
 const ProductsList = () => {
+  const [products,setProducts] = useState([])
+
+
   return (
-    <div className='md:mt-24 mt-[73px] ml-20 md:ml-72 z-20'>ProductsList</div>
+    <div className='md:mt-24 mt-[73px] ml-20 md:ml-72 z-20'>
+      <p className='text-gray-700 font-medium text-lg'>All Product</p>
+    </div>
   )
 }
 
