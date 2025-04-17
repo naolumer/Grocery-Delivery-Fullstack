01bb0c992d2bc97de6baaf34ca49d42283bf2ee8
naolumer <naolumer3@gmail.com>
2025-04-17 13:42:31 +0300

    made the input forms responsives

--- admin/src/components/ImageUploader.jsx
+++ admin/src/components/ImageUploader.jsx
@@ -30,9 +30,9 @@ const ImageUploaderGrid = () => {
   };
 
   return (
-    <div className="space-y-2">
+    <div className="space-y-2 w-[90%] md:w-[85%] xl:w-[40%] lg:w-[60%]">
       <h2 className="font-medium text-gray-700">Product Image</h2>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center md:gap-4 gap-1">
         {images.map((image, index) => (
           <div
             key={index}
--- admin/src/pages/AddProduct.jsx
+++ admin/src/pages/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ImageUploaderGrid from '../components/ImageUploader'
+import { categories } from '../assets/assets'
 
 const AddProduct = () => {
 
@@ -14,7 +15,7 @@ const AddProduct = () => {
 
         <div className=''>
           <p className='font-medium text-gray-700 mb-2 -mt-2'>Product Name</p>
-          <input className='outline-none border border-gray-300 rounded w-[40%] py-2 px-3 text-gray-600' 
+          <input className='outline-none border border-gray-300 rounded w-[85%] md:w-[70%] lg:w-[60%] xl:w-[40%] py-2 px-3 text-gray-600' 
           type="text" 
           placeholder='Type here'/>
         </div>
@@ -22,9 +23,28 @@ const AddProduct = () => {
         <div className=''>
           <p className='font-medium text-gray-700 mb-2'>Product Description</p>
           <textarea 
-          className='w-[40%] px-3 outline-none border border-gray-300 rounded py-2 text-gray-600'
+          className='w-[85%] md:w-[70%] xl:w-[40%] lg:w-[60%] px-3 outline-none border border-gray-300 rounded py-2 text-gray-600'
           rows={4}
           placeholder='Type here'/>
+        </div>
+
+        <div className=''>
+        <label className="block font-medium text-gray-700 mb-1">
+            Category
+          </label>
+          <select
+            className="outline-none border border-gray-300 rounded md:w-[70%] w-[85%] lg:w-[60%] xl:w-[40%]  py-3 px-3 text-gray-700 
+            focus:border-blue-400 focus:ring-2 focus:ring-blue-200"
+          >
+            <option value="" disabled selected hidden>
+              Select a category
+            </option>
+            {categories?.map((cat, ind) => (
+              <option key={ind} value={cat.path}>
+                {cat.path}
+              </option>
+            ))}
+          </select>
         </div>
       </form>
     </div>
