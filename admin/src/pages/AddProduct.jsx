097555b33954d7ddd21e1533fb83f98eaf4939c1
naolumer <naolumer3@gmail.com>
2025-04-17 14:12:39 +0300

    add product UI completed

--- admin/src/pages/AddProduct.jsx
+++ admin/src/pages/AddProduct.jsx
@@ -15,7 +15,7 @@ const AddProduct = () => {
 
         <div className=''>
           <p className='font-medium text-gray-700 mb-2 -mt-2'>Product Name</p>
-          <input className='outline-none border border-gray-300 rounded w-[85%] md:w-[70%] lg:w-[60%] xl:w-[40%] py-2 px-3 text-gray-600' 
+          <input className='outline-none border border-gray-300 rounded w-[90%] md:w-[70%] lg:w-[60%] xl:w-[40%] py-2 px-3 text-gray-600' 
           type="text" 
           placeholder='Type here'/>
         </div>
@@ -23,7 +23,7 @@ const AddProduct = () => {
         <div className=''>
           <p className='font-medium text-gray-700 mb-2'>Product Description</p>
           <textarea 
-          className='w-[85%] md:w-[70%] xl:w-[40%] lg:w-[60%] px-3 outline-none border border-gray-300 rounded py-2 text-gray-600'
+          className='w-[90%] md:w-[70%] xl:w-[40%] lg:w-[60%] px-3 outline-none border border-gray-300 rounded py-2 text-gray-600'
           rows={4}
           placeholder='Type here'/>
         </div>
@@ -33,7 +33,7 @@ const AddProduct = () => {
             Category
           </label>
           <select
-            className="outline-none border border-gray-300 rounded md:w-[70%] w-[85%] lg:w-[60%] xl:w-[40%]  py-3 px-3 text-gray-700 
+            className="outline-none border border-gray-300 rounded md:w-[70%] w-[90%] lg:w-[60%] xl:w-[40%]  py-3 px-3 text-gray-700 
             focus:border-blue-400 focus:ring-2 focus:ring-blue-200"
           >
             <option value="" disabled selected hidden>
@@ -46,6 +46,27 @@ const AddProduct = () => {
             ))}
           </select>
         </div>
+
+        <div className="w-[90%] md:w-[70%] lg:w-[60%] xl:w-[40%] flex gap-4">
+          <div className="flex-1">
+            <p className="text-gray-700 font-medium mb-1">Product price</p>
+            <input
+              className="outline-none border border-gray-300 py-2 px-3 rounded w-full text-gray-600"
+              type="number"
+              placeholder="0"
+            />
+          </div>
+          <div className="flex-1">
+            <p className="text-gray-700 font-medium mb-1">Offer price</p>
+            <input
+              className="outline-none border border-gray-300 py-2 px-3 rounded w-full text-gray-600"
+              type="number"
+              placeholder="0"
+            />
+          </div>
+        </div>
+
+        <button className='bg-[#3bbe85] font-semibold text-white rounded px-4 py-[10px] max-w-28 mb-3'>ADD</button>
       </form>
     </div>
   )
