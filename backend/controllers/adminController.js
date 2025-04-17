d9d2ba5eb9e79e5972e54ce2226715b9d1252330
naolumer <naolumer3@gmail.com>
2025-04-17 15:28:15 +0300

    i modified authAdmin middleware

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -83,6 +83,7 @@ export const addFoodController = async (req,res)=> {
   }
 
   try {
+
     
 
   } catch(error){
--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -1,23 +1,31 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 export const adminAuth = async (req, res, next) => {
-    try {
-        const atoken = req.headers['authorization']?.split(" ")[1];
+  try {
+    const atoken = req.headers['authorization']?.split(" ")[1];
 
-        if (!atoken) {
-            return res.status(401).json({
-                success: false,
-                message: "Not Authorized - No token provided"
-            });
-        }
-
-        const decodedToken = jwt.verify(atoken, process.env.JWT_SECRET);
-        next();
-        
-    } catch (error) {
-        return res.status(401).json({
-            success: false,
-            message: "Not Authorized - " + error.message
-        });
+    if (!atoken) {
+      return res.json({
+        success: false,
+        message: "Not Authorized - No token provided"
+      });
     }
+
+    const decodedToken = jwt.verify(atoken, process.env.JWT_SECRET);
+    req.user = decodedToken;
+
+    if (decodedToken.email !== process.env.ADMIN_EMAIL) {
+      return res.json({
+        success: false,
+        message: "Not Authorized - Admins only"
+      });
+    }
+    next();
+
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Not Authorized - " + error.message
+    });
+  }
 };
