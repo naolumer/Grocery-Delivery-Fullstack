75c2f3ed864a60104a8dffca1de368145dcc7d61
naolumer <naolumer3@gmail.com>
2025-04-18 11:04:00 +0300

    corrected id in toggle instock function

--- admin/src/pages/ProductsList.jsx
+++ admin/src/pages/ProductsList.jsx
@@ -35,21 +35,20 @@ const ProductsList = () => {
     }
   }, [aToken]);
 
-  // Handle toggle switch change
   const handleToggle = (id) => {
     setProducts((prevProducts) =>
       prevProducts.map((product) =>
-        product.id === id ? { ...product, inStock: !product.inStock } : product
+        product._id === id ? { ...product, inStock: !product.inStock } : product
       )
     );
   };
 
   return (
-    <div className="min-h-screen p-6 md:mt-24 mt-[73px] ml-4 md:ml-72 z-20 mb-20 ">
-      <p className="text-gray-700 font-medium text-lg mb-4 ml-12 md:ml-5">All Products</p>
+    <div className="min-h-screen p-6 md:mt-20 mt-[73px] ml-4 md:ml-72 z-20 mb-20 ">
+      <p className="text-gray-600 font-medium text-xl mb-6 ml-12 md:ml-2">All Products</p>
       <div className="bg-white shadow-md rounded-lg overflow-hidden ml-6 md:ml-0 lg:w-[80%]">
 
-        <div className=" grid md:grid-cols-[2fr_1fr_1fr_1fr] grid-cols-[1.5fr_1fr_1fr_1fr] gap-4 bg-gray-50 border-b border-gray-200 font-semibold text-gray-700 p-4">
+        <div className=" grid md:grid-cols-[2fr_1fr_1fr_1fr] grid-cols-[1.5fr_1fr_1fr_1fr] gap-4 bg-gray-100 border-b border-gray-200 font-semibold text-gray-700 p-4">
           <div>Product</div>
           <div>Category</div>
           <div><span className='md:inline-block hidden'>Selling</span>Price </div>
@@ -77,16 +76,16 @@ const ProductsList = () => {
                   <input
                     type="checkbox"
                     checked={product.inStock}
-                    onChange={() => handleToggle(product.id)}
+                    onChange={() => handleToggle(product._id)}
                     className="sr-only"
                   />
                   <div
-                    className={`w-10 h-5 rounded-full transition-colors duration-200 ease-in-out ${
+                    className={`w-12 h-7 rounded-full transition-colors duration-200 ease-in-out ${
                       product.inStock ? 'bg-blue-600' : 'bg-gray-300'
                     }`}
                   >
                     <div
-                      className={`w-4 h-4 bg-white rounded-full shadow-md mt-[2px]  transform transition-transform duration-200 ease-in-out ${
+                      className={`w-5 h-5 bg-white rounded-full shadow-md mt-[4px]  transform transition-transform duration-200 ease-in-out ${
                         product.inStock ? 'translate-x-6' : 'translate-x-0'
                       }`}
                     ></div>
