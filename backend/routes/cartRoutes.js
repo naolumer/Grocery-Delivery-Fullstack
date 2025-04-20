dbccf60cf518a3b382790b0cc00cdc941dfa9faa
naolumer <naolumer3@gmail.com>
2025-04-20 09:44:30 +0300

    created routes for cart controller functions

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -5,6 +5,7 @@ import { userAuth } from "../middlewares/authUser.js"
 
 const cartRouter = express.Router()
 
-cartRouter.get("/get-cart",userAuth, cartController.getCart )
-cartRouter.delete("/delete")
-cartRouter.post("/add")
+cartRouter.get("/get-cart", cartController.getCart )
+cartRouter.delete("/delete/:itemId",cartController.removeItem)
+cartRouter.post("/add",cartController.addItem)
+cartRouter.put("/update",cartController.updateQuantity)
